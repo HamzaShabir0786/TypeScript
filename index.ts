97f13551b35f8b6a4ac1997511a0dc3b4b8ab121
Alexder <alexdergrahab22@gmai.com>
2024-03-20 16:05:00 +0500

    updated project

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-import inquerir from "inquirer"
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second numbe", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtranction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("invalid operator");
+}
